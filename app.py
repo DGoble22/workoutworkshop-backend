eca9e3f38009e660e86859dd608acf055358c3a5
Alexis Mulvany <alexismulvany@gmail.com>
2026-05-04 23:45:35 -0400

    Changes to app.py to match environment variables.

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ Swagger(app)
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT', '24183')
 db_name = os.getenv('DB_NAME')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'change-me-in-production')
 
 db = SQLAlchemy(app)
